d22828e31099099ad173650ebd5b781f933fddb8
Louis Fanien <louis.fanien@gmail.com>
2017-08-09 17:47:37 +0200

    sans front, bloqué 2h30 sur une ligne :'(

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:create, :new]
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :restaurant_id)
+  end
+end
+
